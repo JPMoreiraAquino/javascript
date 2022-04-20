53f39e8cd7015e0422c87509173f2dad45d12336
João Pedro <jp02120123@gmail.com>
2022-04-20 11:48:32 -0300

    feat: Operators

---
+++ JavascriptOperators/operators.js
@@ -0,0 +1,8 @@
+var var1 = 'true' == true;                 // false
+var var2 = '0' == false;                   // true
+var var3 = null == undefined;              // true
+var var4 = false == null;                  // false
+var var5 = null == 0;                      // false
+var var6 = '' == 0;                        // true
+var var7 = '\t\n' == 0;                    // true
+var var8 = '1' == true;                    // true
