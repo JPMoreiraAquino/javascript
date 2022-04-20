f204fe0acccdf5eaf9a8b710618aef756f5f026e
João Pedro <jp02120123@gmail.com>
2022-04-20 11:20:18 -0300

    JavaScript projects

---
+++ javascriptDates/date.js
@@ -0,0 +1,9 @@
+const date = new Date('2022-04-19');
+
+const getYear = date.getFullYear();
+const getMonth = date.getMonth();
+const getDay = date.getDate();
+
+console.log(getYear);
+console.log(getMonth + 1);
+console.log(getDay + 1 );
---
+++ javascriptStrings/strings.js
@@ -0,0 +1,5 @@
+const string = '4';
+
+const soma = string.slice(1, 2)
+
+console.log(soma)
