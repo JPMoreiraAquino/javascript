2ff745a5d015edb44bc1f256b8bec94e3c9f25d0
João Pedro <jp02120123@gmail.com>
2022-04-20 11:50:07 -0300

    feat: more Operators

--- JavascriptOperators/operators.js
+++ JavascriptOperators/operators.js
@@ -6,3 +6,17 @@ var var5 = null == 0;                      // false
 var var6 = '' == 0;                        // true
 var var7 = '\t\n' == 0;                    // true
 var var8 = '1' == true;                    // true
+
+var Atribuição = "de Atribuição'x = y === x = y'"
+var Atribuição = "de adição	x += y	x = x + y"
+var Atribuição = "de subtração	x -= y	x = x - y"
+var Atribuição = "de multiplicação	x *= y	x = x * y"
+var Atribuição = "de divisão	x /= y	x = x / y"
+var Atribuição = "de resto	x %= y	x = x % y"
+var Atribuição = "exponencial	x **= y	x = x ** y"
+var Atribuição = "bit-a-bit por deslocamento á esquerda	x <<= y	x = x << y"
+var Atribuição = "bit-a-bit por deslocamento á direita	x >>= y	x = x >> y"
+var Atribuiçãode = "bit-a-bit deslocamento á direita não assinado	x >>>= y	x = x >>> y"
+var Atribuição = "AND bit-a-bit	x &= y	x = x & y"
+var Atribuição = "XOR bit-a-bit	x ^= y	x = x ^ y"
+var Atribuição = "OR bit-a-bit	x |= y	x = x | y"
